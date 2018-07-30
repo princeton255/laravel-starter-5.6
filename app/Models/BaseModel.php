193f5cbee9392417f1fd94d9adf89e401d410d5c
Tumaini Mosha <mtumaini@gateway.co.tz>
2018-07-30 17:31:58 +0300

    Update Laravel boilerplate with starter commands and classes

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property \Carbon\Carbon $deleted_at
+ */
+class BaseModel extends Model
+{
+    //
+}
